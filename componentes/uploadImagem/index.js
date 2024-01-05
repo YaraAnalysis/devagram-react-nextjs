0682c5aaaaa28718d5127afd9682d6cf364f44fb
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-01-05 00:13:22 -0300

    Add uploadImagem components - part 2

--- componentes/uploadImagem/index.js
+++ componentes/uploadImagem/index.js
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 
 export function UploadImagem({
-    className = ''
+    className = '',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName = ''
 }) {
     const referenciaInput = useRef(null);
 
@@ -9,14 +12,44 @@ export function UploadImagem({
         referenciaInput?.current?.click();
     }
 
+    const aoAlterarImagem = () => {
+        console.log('aoAlterarImagem')
+
+        if (!referenciaInput?.current?.files?.length) {
+            return;
+        }
+
+        const arquivo = referenciaInput?.current?.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(arquivo);
+        fileReader.onloadend = () => {
+            setImagem({
+                preview: fileReader.result,
+                arquivo
+            });
+        }
+
+    }
+
     return (
         <div className={`uploadImagemContainer ${className}`} onClick={abrirSeletorArquivos}>
             <button>abrir seletor de arquivos</button>
+            {imagemPreview && (
+                <div className="imagemPreviewConatiner">
+                    <img
+                        src={imagemPreview}
+                        alt='imagemPreview'
+                        className={imagemPreviewClassName}
+                    />
+                </div>
+            )}
+
             <input 
                 type='file' 
                 className='oculto' 
                 accept="image/*"
                 ref={referenciaInput}
+                onChange={aoAlterarImagem}
             />
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,19 @@ import Image from 'next/image'
 import Botao from '@/componentes/botao'
 import Avatar from '@/componentes/avatar'
 import { UploadImagem } from '@/componentes/uploadImagem'
+import { useState } from 'react'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+
+  console.log(imagem);
+
   return (
     <>
       <h1>Olá Mundo!</h1>
-      <UploadImagem />
+      <UploadImagem setImagem={setImagem} imagemPreview={imagem?.preview}/>
       <div style={{width: 200}}>
         <Avatar />
         <Botao texto={'Login'} cor='invertido' manipularclique={() => console.log('Botão clicado')} />
