363c1de9cda1f304b211bd29d8f912a6b8bba3f8
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-02-20 23:11:55 -0300

    FEAT: Enhance login and signup logic

--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -3,18 +3,25 @@ import HttpService from "./HttpService";
 export default class UsuarioService extends HttpService {
     async login(credenciais) {
         const  { data } = await this.post('/login', credenciais);
-        console.log(data);
+        console.log(data);  // apagar esse console.log
 
         localStorage.setItem("nome", data.nome);
         localStorage.setItem("email", data.email);
         localStorage.setItem("token", data.token);
 
-        if (data.avatar) {
-            localStorage.setItem("avatar", data.avatar);
+        const usuario = await this.get('/usuario');
+        localStorage.setItem('id', usuario.data._id);
+
+        if (usuario.data.avatar) {
+            localStorage.setItem("avatar", usuario.data.avatar);
         }
     }
 
     async cadastro(dados) {
         return this.post('cadastro', dados);
+    }
+
+    estaAutenticado() {
+        return localStorage.getItem('token') !== null;
     }
 }
