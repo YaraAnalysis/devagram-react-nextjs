7d5a510ccec13d8e27c08b1d00aac1f1519fe52f
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-02-21 15:26:26 -0300

    FEAT: Implement navbar component structure

--- componentes/layout/Cabecalho.js
+++ componentes/layout/Cabecalho.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import logoHorizontalImg from '../../public/images/logoHorizontal.svg';
 import imagemLupa from '../../public/images/lupa.svg';
+import Navegacao from './Navegacao';
 
 export default function Cabecalho() {
     return (
@@ -29,8 +30,9 @@ export default function Cabecalho() {
                         value={''}
                         onChange={() => consolelog('pesquisando')}
                         />
-
                 </div>
+
+                <Navegacao className='desktop' />
             </div>
 
         </header>
---
+++ componentes/layout/Navegacao.js
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import imgHomeAtivo from '../../public/images/homeAtivo.svg';
+import imgHomeCinza from '../../public/images/homeCinza.svg';
+import imgPublicacaoAtiva from '../../public/images/publicacaoAtiva.svg';
+import imgPublicacaoCinza from '../../public/images/publicacaoCinza.svg';
+import imgUsuarioAtivo from '../../public/images/usuarioAtivo.svg';
+import imgUsuarioCinza from '../../public/images/usuarioCinza.svg';
+
+export default function Navegacao({ className }) {
+    return (
+        <nav className={`barraNavegacao ${className}`}>
+            <ul>
+                <li>
+                    <Image
+                        src={imgHomeAtivo}
+                        alt='icone home'
+                        width={20}
+                        height={20}
+                    />
+                </li>
+
+                <li>
+                    <Image
+                        src={imgPublicacaoCinza}
+                        alt='icone publicacao'
+                        width={20}
+                        height={20}
+                    />
+                </li>
+
+                <li>
+                    <Image
+                        src={imgUsuarioCinza}
+                        alt='icone usuario'
+                        width={20}
+                        height={20}
+                    />
+                </li>
+
+            </ul>
+        </nav>
+    );
+}
