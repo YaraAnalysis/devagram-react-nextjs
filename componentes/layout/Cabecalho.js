b5b18fedd00bc8a77edc67b3d62cdc4a126b3f6d
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-02-24 23:44:28 -0300

    feat: Add search component structure

--- componentes/layout/Cabecalho.js
+++ componentes/layout/Cabecalho.js
@@ -3,8 +3,46 @@ import { useState } from 'react';
 import logoHorizontalImg from '../../public/images/logoHorizontal.svg';
 import imagemLupa from '../../public/images/lupa.svg';
 import Navegacao from './Navegacao';
+import ResultadoPesquisa from './ResultadoPesquisa';
 
 export default function Cabecalho() {
+    const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+    const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+    const aoPesquisar = (e) =>{
+        setTermoPesquisado(e.target.value);
+        setResultadoPesquisa([]);
+
+        if (termoPesquisado.length < 3) {
+            return;
+        }
+
+        setResultadoPesquisa([
+            {
+                avatar: '',
+                nome: 'Yara',
+                email: 'yara@devagram.com',
+                _id: '3242432'
+            },
+            {
+                avatar: '',
+                nome: 'Douglas',
+                email: 'douglas@devagram.com',
+                _id: '65487789'
+            },
+            {
+                avatar: '',
+                nome: 'Rafael',
+                email: 'rafael@devagram.com',
+                _id: '78958463'
+            }
+        ])
+    }
+
+    const aoClicarResultadoPesquisa = () => {
+        console.log('aoClicarResultadoPesquisa', {id});
+    }
+
     return (
         <header className='cabecalhoPrincipal'>
             <div className='conteudoCabecalhoPrincipal'>
@@ -28,13 +66,28 @@ export default function Cabecalho() {
                     <input
                         type='text'
                         placeholder='Pesquisar'
-                        value={''}
-                        onChange={() => consolelog('pesquisando')}
+                        value={termoPesquisado}
+                        onChange={aoPesquisar}
                     />
                 </div>
 
                 <Navegacao className='desktop' />
             </div>
+
+            {resultadoPesquisa.length > 0 && (
+                <div className='resultadoPesquisaConatiner'>
+                    {resultadoPesquisa.map(r => (
+                        <ResultadoPesquisa
+                            avatar={r.avatar}
+                            nome={r.nome}
+                            email={r.email}
+                            key={r._id}
+                            id={r._id}
+                            onClick={aoClicarResultadoPesquisa}
+                        />
+                    ))}
+                </div>
+            )}
         </header>
     );
 }
