cb842c30db53ec27013ec1b7cab3846d3064a7b0
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-02-27 02:22:31 -0300

    STYLE: Implements the post style - part 1

--- componentes/feed/Postagem.js
+++ componentes/feed/Postagem.js
@@ -1,5 +1,72 @@
-export default function Postagem(props) {
+import Link from "next/link";
+import Avatar from "../avatar";
+import Image from "next/image";
+
+import imgCurtir from '../../public/images/curtir.svg';
+import imgCurtido from '../../public/images/curtido.svg';
+import imgComentarioAtivo from '../../public/images/comentarioAtivo.svg';
+import imgComentarioCinza from '../../public/images/comentarioCinza.svg';
+
+export default function Postagem({
+    usuario,
+    fotoDoPost,
+    descricao,
+    comentarios
+}) {
+    
     return (
-        <h1>Post</h1>
+        <div>
+            <Link href={`/perfil/${usuario.id}`}>
+                <section className="cabecalhoPostagem">
+                    <Avatar src={usuario.avatar} />
+                    <strong>{usuario.nome}</strong>
+                </section>
+            </Link>
+
+            <div className="fotoDaPostagem">
+                <img src={fotoDoPost} alt='foto d postagem' />
+            </div>
+
+            <div className="rodapeDaPostagem">
+                <div className="acoesDaPostagem">
+                    <Image
+                        src={imgCurtir}
+                        alt='icone curtir'
+                        width={20}
+                        height={20}
+                        onClick={() => console.log('curtir')}
+                    />
+
+                    <Image
+                        src={imgComentarioCinza}
+                        alt='icone comentar'
+                        width={20}
+                        height={20}
+                        onClick={() => console.log('comentar')}
+                    />
+
+                    <span className="quantidadeCurtidas">
+                        Curtido por <strong>32 pessoas</strong>
+                    </span>
+                </div>
+
+                <div className="descricaoDaPostagem">
+                    <strong className="nomeUsuario">{usuario.nome}</strong>
+                    <p className="descricao">
+                        {descricao}
+                    </p>
+                </div>
+            </div>
+
+            <div className="comentariosDaPublicacao">
+                {comentarios.map((comentario, i) => (
+                    <div className="comentario" key={i}>
+                        <strong className="nomeUsuario">{comentario.nome}</strong>
+                        <p className="descricao">{comentario.mensagem}</p>
+                    </div>
+
+                ))}
+            </div>
+        </div>
     );
 }
--- componentes/feed/index.js
+++ componentes/feed/index.js
@@ -11,16 +11,24 @@ export function Feed({ usuarioLogado }) {
                 id: '1',
                 usuario: {
                     id: '1',
-                    nome: 'Yara',
+                    nome: 'Yara Vasconcellos',
                     avatar: null,
                 },
-                fotoDoPost: '',
-                descricao: '',
+                fotoDoPost: 'https://img.freepik.com/fotos-gratis/aguia-careca-majestosa-empoleirada-olhando-fixamente-com-olhos-afiados-gerados-por-ia_188544-35430.jpg',
+                descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make',
                 curtidas: [],
                 comentarios: [
                     {
                         nome: 'Fulano',
                         mensagem: 'Muito legal!'
+                    },
+                    {
+                        nome: 'Fulano de tal',
+                        mensagem: 'Imagem muito bacana!'
+                    },
+                    {
+                        nome: 'Fulanoda esquina',
+                        mensagem: 'Isso aí! Continue assim...!'
                     }
                 ]
             },
@@ -28,11 +36,11 @@ export function Feed({ usuarioLogado }) {
                 id: '2',
                 usuario: {
                     id: '2',
-                    nome: 'Douglas',
+                    nome: 'Douglas Oliveira',
                     avatar: null,
                 },
-                fotoDoPost: '',
-                descricao: '',
+                fotoDoPost: 'https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg',
+                descricao: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
                 curtidas: [],
                 comentarios: [
                     {
