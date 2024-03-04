a671b56adc4f23f815172815f6aae25a3e71dd23
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-03-04 19:21:03 -0300

    Update index.js

--- componentes/feed/index.js
+++ componentes/feed/index.js
@@ -31,51 +31,6 @@ export function Feed({ usuarioLogado }) {
             ));
 
             setListaDePostagens(postagensFormatadas);
-
-            /*setListaDePostagens([
-                {
-                    id: '1',
-                    usuario: {
-                        id: '1',
-                        nome: 'Yara Vasconcellos',
-                        avatar: null,
-                    },
-                    fotoDoPost: 'https://img.freepik.com/fotos-gratis/aguia-careca-majestosa-empoleirada-olhando-fixamente-com-olhos-afiados-gerados-por-ia_188544-35430.jpg',
-                    descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.',
-                    curtidas: [],
-                    comentarios: [
-                        {
-                            nome: 'Fulano',
-                            mensagem: 'Muito legal!'
-                        },
-                        {
-                            nome: 'Fulano de tal',
-                            mensagem: 'Imagem muito bacana!'
-                        },
-                        {
-                            nome: 'Fulano da esquina',
-                            mensagem: 'Isso aí! Continue assim...!'
-                        }
-                    ]
-                },
-                {
-                    id: '2',
-                    usuario: {
-                        id: '2',
-                        nome: 'Douglas Oliveira',
-                        avatar: null,
-                    },
-                    fotoDoPost: 'https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg',
-                    descricao: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.',
-                    curtidas: [],
-                    comentarios: [
-                        {
-                            nome: 'Ciclano',
-                            mensagem: 'Muito bom!'
-                        }
-                    ]
-                }
-            ])*/
         }
         fetchData();
     }, [usuarioLogado]);
